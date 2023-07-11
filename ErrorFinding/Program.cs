4eedbffed63a21bdaa92a07d8ede5cd2d69c11d9
OttomanBull <kayhan1901@gmail.com>
2023-07-11 11:36:12 +0300

    aa

--- ErrorFinding/Program.cs
+++ ErrorFinding/Program.cs
@@ -11,7 +11,7 @@ class Program
     {
         ErrorListDal errorListDal = new ErrorListDal();
         errorListDal.GetErrorListApi();
-        Console.WriteLine("test 1 2 3 ");
+        Console.WriteLine("test 1 2 3 4");
       
     }
 }
