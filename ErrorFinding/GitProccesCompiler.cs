40ae4f81baf449278f9bf9b59022e45309816fdc
OttomanBull <kayhan1901@gmail.com>
2023-07-27 15:57:57 +0300

    GitProccesCompiler oluşturuldu

---
+++ ErrorFinding/GitProccesCompiler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using System.Management.Automation;
+
+namespace ErrorFinding
+{
+    public class GitProccesCompiler
+    {
+        public void deneme() 
+        {
+            string directory = "C:\\Users\\Work and Study\\Documents\\GitHub\\ErrorSynchronizationProject"; // directory of the git repository
+
+            using (PowerShell powershell = PowerShell.Create())
+            {
+                // this changes from the user folder that PowerShell starts up with to your git repository
+                powershell.AddScript($"cd {directory}");
+
+                powershell.AddScript(@"git checkout master");
+
+                powershell.AddScript(@"git pull");
+
+                Collection<PSObject> results = powershell.Invoke();
+            }
+        }
+    }
+}
