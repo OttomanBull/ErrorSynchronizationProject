f267999b5b2d48eed199af9b008bed90ae1a0d11
OttomanBull <kayhan1901@gmail.com>
2023-07-31 13:29:22 +0300

    UI ve Management error codeları işlemlerin yapılabilmesi için proje içine yüklendi

--- ErrorFinding/GitProccesCompiler.cs
+++ ErrorFinding/GitProccesCompiler.cs
@@ -1,25 +1,43 @@
 ﻿using System.Collections.ObjectModel;
+using System.IO;
 using System.Management.Automation;
 
 namespace ErrorFinding
 {
     public class GitProccesCompiler
     {
-        public void deneme() 
+        private void PowerShellGitOperations(string directory, string fileName)
         {
-            string directory = "C:\\Users\\Work and Study\\Documents\\GitHub\\ErrorSynchronizationProject"; // directory of the git repository
+            string name = "Bahadır";
+            int value = 10;
+            string lastName = name + value;
+            string githubSSHURL = "git@github.com:OttomanBull/ErrorSynchronizationProject.git";
 
             using (PowerShell powershell = PowerShell.Create())
             {
-                // this changes from the user folder that PowerShell starts up with to your git repository
                 powershell.AddScript($"cd {directory}");
 
-                powershell.AddScript(@"git checkout master");
+                if (File.Exists(Path.Combine(directory, fileName)))
+                    powershell.AddScript($"git clone {githubSSHURL}");
 
+                powershell.AddScript(@"git checkout bahadirv3");
                 powershell.AddScript(@"git pull");
+                powershell.AddScript($"git checkout -b {lastName}");
 
                 Collection<PSObject> results = powershell.Invoke();
             }
         }
+
+        public void deneme() 
+        {
+            string fileName =  @"ErrorFinding.sln";
+            string directory = @"C:\Users\Work and Study\Documents\GitHub\ErrorSynchronizationProject";
+
+            PowerShellGitOperations(directory, fileName);
+        }
+
+        
+        
+        
     }
 }
--- ErrorFinding/Program.cs
+++ ErrorFinding/Program.cs
@@ -12,12 +12,13 @@ class Program
     static void Main()
     {
 
-        ToFolder toFolder = new ToFolder();
-        string managementText=toFolder.ToManagementFile("https://raw.githubusercontent.com/xyztek/CrowdFundingManagement/master/src/lang/errorCodes/tr_TR.js");
-        JToken uiJson = toFolder.toUi("https://raw.githubusercontent.com/xyztek/CrowdFundingUI/master/src/shared/language/english.json");
-        Console.WriteLine(uiJson);
+        //ToFolder toFolder = new ToFolder();
+        //string managementText=toFolder.ToManagementFile("https://raw.githubusercontent.com/xyztek/CrowdFundingManagement/master/src/lang/errorCodes/tr_TR.js");
+        //JToken uiJson = toFolder.toUi("https://raw.githubusercontent.com/xyztek/CrowdFundingUI/master/src/shared/language/english.json");
+        //Console.WriteLine(uiJson);
         //Console.WriteLine(managementText);
-
+        GitProccesCompiler compiler = new GitProccesCompiler();
+        compiler.deneme();
     }
 }
 
